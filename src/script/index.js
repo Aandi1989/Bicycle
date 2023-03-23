a3e88a33aa98ab5193dcb281b82ad45f8aa9776f
Aandi1989 <navaliny1989@gmail.com>
2023-03-23 21:47:17 +0300

    mainScreen bg

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,24 @@
-$(document).ready(function(){
+function ibg(){
+    $.each($('.ibg'), function(index, val){
+        if($(this).find('img').length>0){
+            $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+        }
+    });
+}
+ibg();
 
-    $('#button').on('click', function(){
-        alert('Hello, it is jQuery!')
-    })
+/*
+Чистый JS
+function ibg(){
 
-})
+let ibg=document.querySelectorAll(".ibg");
+for (var i = 0; i < ibg.length; i++) {
+if(ibg[i].querySelector('img')){
+ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+}
+}
+}
+
+ibg();
+*/
+    
